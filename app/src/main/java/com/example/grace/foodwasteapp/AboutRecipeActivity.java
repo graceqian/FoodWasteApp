62264a1e3d1665b7c01b8fb3fd6bb855e5c8d75c
graceqian <graceqian125@gmail.com>
2017-06-04 14:38:38 -0400

    users can now search their own queries, now links them out to recipe

---
+++ app/src/main/java/com/example/grace/foodwasteapp/AboutRecipeActivity.java
@@ -0,0 +1,83 @@
+package com.example.grace.foodwasteapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class AboutRecipeActivity extends AppCompatActivity {
+
+    private List<String> recipeIngredientList;
+
+    //TODO WHEN A RECIPE IS CLICKED, AN INTENT IS PASSED HERE WITH THE MATCH INFORMATION
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_recipe);
+
+        Intent intent = getIntent();
+        recipeIngredientList = intent.getStringArrayListExtra("ingredients");
+
+        //retrieve and display information about the match (recipe)
+        ((TextView)findViewById(R.id.tvAboutRecipeName)).setText(intent.getStringExtra("name"));//recipe name
+        ((TextView)findViewById(R.id.tvAboutRecipeTime)).setText(intent.getStringExtra("time"));//recipe time
+        ((TextView)findViewById(R.id.tvAboutRecipeRating)).setText("Rating: " + intent.getStringExtra("rating"));//recipe rating
+        ((TextView)findViewById(R.id.tvIngredientsInRecipe)).setText(recipeIngredientList.size() + " Ingredients");//recipe rating
+        //TODO figure out why its null, if you cant, jsut get rid fo this
+        ((TextView)findViewById(R.id.tvRecipeCuisine)).setText("Cuisine: " + intent.getStringExtra("cuisine"));//recipe cuisine
+
+
+        //build adapter
+        ArrayAdapter<String> adapter = new RecipeIngredientAdapter();
+
+        //config list view
+        ListView list = (ListView)findViewById(R.id.lvRecipeIngredients);
+        list.setAdapter(adapter);
+
+    }
+
+    private class RecipeIngredientAdapter extends ArrayAdapter<String> {
+        //https://www.youtube.com/watch?v=3k3CunDZpFk
+        //https://www.youtube.com/watch?v=WRANgDgM2Zg DrBFraser
+        public RecipeIngredientAdapter() {
+            super(AboutRecipeActivity.this, R.layout.list_recipe_ingredients, recipeIngredientList);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // Make sure we have a view to work with (may have been given null)
+            View itemView = convertView;
+            if (itemView == null) {
+                itemView = getLayoutInflater().inflate(R.layout.list_recipe_ingredients, parent, false);//create a new view
+                //inflater takes a piece of xml code and inflates into an object to be displayed on the screen
+            }
+
+            //populating the list
+
+            // Find the ingredient to work with.
+            String currentIngredient = recipeIngredientList.get(position);
+
+            // Fill the view
+
+            // ingredient:
+            TextView ingredientText = (TextView) itemView.findViewById(R.id.tvRecipeIngredient);
+            ingredientText.setText(currentIngredient);
+
+            return itemView;
+        }
+    }
+
+    //When the "make Recipe" button is pressed, the user is redirected to a webpage of the recipe
+    protected void onClickMakeRecipe(View v){
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("http://www.yummly.com/recipe/" + getIntent().getStringExtra("id")));
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/example/grace/foodwasteapp/FindRecipeActivity.java
+++ app/src/main/java/com/example/grace/foodwasteapp/FindRecipeActivity.java
@@ -1,7 +1,9 @@
 package com.example.grace.foodwasteapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SearchView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -9,6 +11,8 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import retrofit.Call;
 import retrofit.Callback;
@@ -20,6 +24,8 @@ public class FindRecipeActivity extends AppCompatActivity {
 
     private ListView lvRecipes;
     private GetRecipeResponse recipeResponse;
+    private final String APP_ID = "26edbdd7";
+    private final String APP_KEY = "e821c0abc1c766b3cd3f2a2c23023113";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,34 +37,80 @@ public class FindRecipeActivity extends AppCompatActivity {
                 addConverterFactory(GsonConverterFactory.create());
 
         Retrofit retrofit = builder.build();
-        YummlyClient.YummlyApiInterface client = retrofit.create(YummlyClient.YummlyApiInterface.class);
-        Call<GetRecipeResponse> call = client.getRecipeResponse("26edbdd7", "e821c0abc1c766b3cd3f2a2c23023113","onion+soup");
-//        Toast.makeText(FindRecipeActivity.this, client.getRecipeResponse("soup").request().url().toString() , Toast.LENGTH_SHORT).show();
+        final YummlyClient.YummlyApiInterface client = retrofit.create(YummlyClient.YummlyApiInterface.class);
 
-        //must call api asynchronously because there is a UI thread
-        call.enqueue(new Callback<GetRecipeResponse>() {
+        //handling queries in search bar
+        SearchView searchView = (SearchView) findViewById(R.id.svRecipeSearch);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public void onResponse(Response<GetRecipeResponse> response, Retrofit retrofit) {
-                recipeResponse = response.body();
-                if(recipeResponse == null)
-                    Toast.makeText(FindRecipeActivity.this, "its null" , Toast.LENGTH_SHORT).show();
-
-                //build adapter
-                ArrayAdapter<Match> adapter = new RecipeAdapter();
-
-                //config list view
-                ListView list = (ListView)findViewById(R.id.lvRecipes);
-                list.setAdapter(adapter);
+            public boolean onQueryTextSubmit(String query) {
+                callSearch(query);
+                return true;
             }
 
             @Override
-            public void onFailure(Throwable t) {
-                Toast.makeText(FindRecipeActivity.this, "Something went wrong!", Toast.LENGTH_SHORT).show();
+            public boolean onQueryTextChange(String newText) {
+//              if (searchView.isExpanded() && TextUtils.isEmpty(newText)) {
+                callSearch(newText);
+//              }
+                return true;
             }
+
+            public void callSearch(String query) {
+                Call<GetRecipeResponse> call = client.getRecipeResponse(APP_ID, APP_KEY,query);
+
+                //must call api asynchronously because there is a UI thread
+                call.enqueue(new Callback<GetRecipeResponse>() {
+                    @Override
+                    public void onResponse(Response<GetRecipeResponse> response, Retrofit retrofit) {
+                        recipeResponse = response.body();
+                        if(recipeResponse == null)
+                            Toast.makeText(FindRecipeActivity.this, "its null" , Toast.LENGTH_SHORT).show();
+
+                        //build adapter
+                        ArrayAdapter<Match> adapter = new RecipeAdapter();
+
+                        //config list view
+                        ListView list = (ListView)findViewById(R.id.lvRecipes);
+                        list.setAdapter(adapter);
+                    }
+
+                    @Override
+                    public void onFailure(Throwable t) {
+                        Toast.makeText(FindRecipeActivity.this, "Something went wrong!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
         });
+
+//        //Call<GetRecipeResponse> call = client.getRecipeResponse(APP_ID, APP_KEY,"onion+soup");
+//
+//        //must call api asynchronously because there is a UI thread
+//        call.enqueue(new Callback<GetRecipeResponse>() {
+//            @Override
+//            public void onResponse(Response<GetRecipeResponse> response, Retrofit retrofit) {
+//                recipeResponse = response.body();
+//                if(recipeResponse == null)
+//                    Toast.makeText(FindRecipeActivity.this, "its null" , Toast.LENGTH_SHORT).show();
+//
+//                //build adapter
+//                ArrayAdapter<Match> adapter = new RecipeAdapter();
+//
+//                //config list view
+//                ListView list = (ListView)findViewById(R.id.lvRecipes);
+//                list.setAdapter(adapter);
+//            }
+//
+//            @Override
+//            public void onFailure(Throwable t) {
+//                Toast.makeText(FindRecipeActivity.this, "Something went wrong!", Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
         registerRecipeClickCallback();
     }
+
 
     public class RecipeAdapter extends ArrayAdapter<Match> {
 
@@ -102,17 +154,28 @@ public class FindRecipeActivity extends AppCompatActivity {
         }
     }
 
-    //method is called when the user clicks on a displayed ingredient.
-    //a dialogue appears in which the user can edit or delete the ingredient clicked
-    private void registerClickCallback(){
-        ListView list = (ListView)findViewById(R.id.lvKitchenData);
+    //method is called when the user clicks on a displayed recipe
+    //the app switches to an overview of the recipe
+    private void registerRecipeClickCallback(){
+        ListView list = (ListView)findViewById(R.id.lvRecipes);
         //for click on item
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View viewClicked, int position, long id) {
-
+                Match match = recipeResponse.getMatches().get(position);//match clicked
+                Intent toAboutRecipe = new Intent(FindRecipeActivity.this, AboutRecipeActivity.class);
+                toAboutRecipe.putExtra("name", match.getRecipeName());
+                toAboutRecipe.putExtra("rating", match.getRating() + "");
+                toAboutRecipe.putStringArrayListExtra("ingredients", (ArrayList<String>)match.getIngredients());
+                toAboutRecipe.putExtra("cuisine", (ArrayList)match.getAttributes().getCuisine());
+                toAboutRecipe.putExtra("id", match.getId());
+                int timeInMinutes = match.getTotalTimeInSeconds()/60;
+                int hours = timeInMinutes / 60;
+                int minutes = timeInMinutes % 60;
+                toAboutRecipe.putExtra("time", hours + " hrs " + minutes + " min");
+                startActivity(toAboutRecipe);
             }
-        }
+        });
     }
 }
